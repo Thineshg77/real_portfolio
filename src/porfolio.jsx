6d14bffe70d36f2c937c160864104b68bd91438f
Thineshg77 <thineshganesan34@gmail.com>
2024-08-16 14:21:57 +0530

    8

--- src/porfolio.jsx
+++ src/porfolio.jsx
@@ -13,20 +13,29 @@ import advice from './assets/aa.png'
 import converter from './assets/cc.png'
 import myweb from './assets/mw.png'
 import generator from './assets/pg.png'
-import weather from './assets/wa.png'
+import weather from './assets/wap.png'
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { TbFileCv } from "react-icons/tb";
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { IoMenuSharp } from "react-icons/io5";
 const porfolio = () => {
   return (
     <div className='container'>
       <div className="home">
-        <nav><h1>GT Porfolio</h1></nav> 
+        <nav><h1>Porfolio</h1><h1><a href="#"><IoMenuSharp /></a></h1></nav> 
       </div>
       <div className="intro">
             <h1>HI, I'm Thinesh G</h1>
             <h3>Frontend Developer | Chemist</h3>
-            <p>Lorem ipsum dolor sit amet 
-                consectetur adipisicing elit. Quae cum 
-                in fugit asperiores 
-                neque. Sed ipsa quisquam ex repellendus tempore.</p>
+            <p>As a Front-End Developer and Chemist, I have clear knowledge about both
+                 Chemicals as well as Syntex with little experience</p>
+        </div>
+        <div className="social">
+       <a href="https://wa.me/qr/HZ2QK4D6UQ2JF1"> <AiOutlineWhatsApp /></a>
+       <a href="https://www.linkedin.com/in/thinesh-g-a13a84281?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"> <FaLinkedin /></a>
+        <a href="https://github.com/Thineshg77"><FaGithub /></a>
+       <a href="">Resume <TbFileCv /></a>
         </div>
     <div className="about">
         <img src={profile} alt="my image" />
@@ -34,8 +43,11 @@ const porfolio = () => {
     </div>
     <div className="me">
         <h2>About me</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est voluptatibus omnis voluptates perspiciatis 
-            maxime atque consequuntur vel! Placeat, temporibus aliquid.</p>
+        <p>I'm a Front-End Developer and Chemist like to build a clean and  usable 
+            interface and also I have experience with the Reactjs. I'm the one who 
+            eager to upgrade my skills in every aspects.I have seeking for the opportunity
+            to showcase my skills...
+        </p>
     </div>
     <div className="skills">
         <h2>Skills</h2>
@@ -56,23 +68,48 @@ const porfolio = () => {
         </div>
     </div>
     <div className="project">
-     <h2>Project</h2>   
+     <h2>Projects</h2>   
     <div class="card">
   <img src={dc} alt="Avatar"/>
   <div class="contain">
     <h4><b>Digital Clock Project</b></h4> 
     <p>Architect & Engineer</p>
-    <a href="">&#10230;</a> 
+    <div className="bad">
+    <span class="badge">HTML</span>
+    <span class="badge">CSS</span>
+    <span class="badge">REACTJS</span>
+    </div>
+    <a href="https://thinesh-clock.netlify.app/">&#10230;</a> 
   </div>
   <div/>
-  
 </div>
+<div class="card">
+  <img src={weather} alt="Avatar"/>
+  <div class="contain">
+    <h4><b>Weather App Project</b></h4> 
+    <p>The Weather App is a user-friendly mobile app designed to provide
+        real time and accurate weather information to users
+    </p>
+    <div className="bad">
+    <span class="badge">HTML</span>
+    <span class="badge">CSS</span>
+    <span class="badge">REACTJS</span>
+    </div>
+    <a href="https://weatherappbythinesh.netlify.app/">&#10230;</a>
+  </div>
+  <div/>
+  </div>
 <div class="card">
   <img src={converter} alt="Avatar"/>
   <div class="contain">
     <h4><b>Currency Converter Project </b></h4> 
     <p>Architect & Engineer</p> 
-    <a href="">&#10230;</a>
+    <div className="bad">
+    <span class="badge">HTML</span>
+    <span class="badge">CSS</span>
+    <span class="badge">REACTJS</span>
+    </div>
+    <a href="https://currencyconverterbythinesh.netlify.app/">&#10230;</a>
   </div>
   <div/>
   </div>
@@ -81,7 +118,12 @@ const porfolio = () => {
   <div class="contain">
     <h4><b>Advice Website Project</b></h4> 
     <p>Architect & Engineer</p> 
-    <a href="">&#10230;</a>
+    <div className="bad">
+    <span class="badge">HTML</span>
+    <span class="badge">CSS</span>
+    <span class="badge">REACTJS</span>
+    </div>
+    <a href="https://advice-appproject.netlify.app/">&#10230;</a>
   </div>
   <div/>
   </div>
@@ -90,7 +132,11 @@ const porfolio = () => {
   <div class="contain">
     <h4><b>Advice Website Project</b></h4> 
     <p>Architect & Engineer</p> 
-    <a href="">&#10230;</a> 
+    <div className="bad">
+    <span class="badge">HTML</span>
+    <span class="badge">CSS</span>
+    </div>
+    <a href="https://tn60pencilarts.netlify.app/">&#10230;</a> 
   </div>
   <div/>
   </div>
@@ -99,16 +145,12 @@ const porfolio = () => {
   <div class="contain">
     <h4><b>Passwor Generator Project</b></h4> 
     <p>Architect & Engineer</p>
-    <a href="">&#10230;</a> 
-  </div>
-  <div/>
-  </div>
-  <div class="card">
-  <img src={weather} alt="Avatar"/>
-  <div class="contain">
-    <h4><b>Weather App Project</b></h4> 
-    <p>Architect & Engineer</p>
-    <a href="">&#10230;</a>
+    <div className="bad">
+    <span class="badge">HTML</span>
+    <span class="badge">CSS</span>
+    <span class="badge">REACTJS</span>
+    </div>
+    <a href="https://passwordbythinesh.netlify.app/">&#10230;</a> 
   </div>
   <div/>
   </div>
@@ -131,6 +173,9 @@ const porfolio = () => {
         <button>Submit</button>
       </div>
  </div>
+<div className="us">
+    <p>&copy;Designed by THINESH</p>
+</div>
   </div>
 
   )
